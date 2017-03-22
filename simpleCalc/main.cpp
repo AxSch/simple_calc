0e25f87626c132032bb72b732f43c5085fcff45a
Alexander Schuneman <alex_schuneman@outlook.com>
2017-03-22 11:55:59 +0000

    Add initial hierarchy

--- simpleCalc/main.cpp
+++ simpleCalc/main.cpp
@@ -4,12 +4,17 @@
 //
 //  Created by Korrol Schuneman on 22/03/2017.
 //  Copyright © 2017 Korrol Alexander Schuneman. All rights reserved.
+//  main.cpp
+//  Chapter6Exercise2
 //
+//  Created by Korrol Schuneman on 20/03/2017.
+//  Copyright © 2017 Korrol Alexander Schuneman. All rights reserved.
+//  Simple calculator to implement a set of grammar rules
 
-#include <iostream>
 
-int main(int argc, const char * argv[]) {
-	// insert code here...
-	std::cout << "Hello, World!\n";
+#include "std_lib_facilities.h"
+
+int main() {
+	
     return 0;
 }
